421a1ec899ff1f00955116aeccaf18d7650643c0
YogaVicky <yogasrivarshan@gmail.com>
2019-05-12 21:03:13 +0530

    update

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,17 +1,26 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from flaskblog.models import User
+from wtforms.validators import ValidationError
 class RegistrationForm(FlaskForm):
     username=StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
     email=StringField('Email',validators=[DataRequired(),Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),EqualTo('password')])
     submit=SubmitField('SignUp')
-
+    def validate_username(self,username):
+        user=User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken.Please choose a different name')
+    def validate_email(self,email):
+        user=User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken.Please choose a different email')
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+    
