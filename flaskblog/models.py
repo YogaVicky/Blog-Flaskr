3f6210005c39de4b1a424b94967368de8697256f
YogaVicky <yogasrivarshan@gmail.com>
2019-05-25 16:42:01 +0530

    update

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,6 @@
-from flaskblog import db,login_manager
+from flaskblog import db,login_manager,app
 from datetime import datetime
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask_login import UserMixin
 
 @login_manager.user_loader
@@ -10,13 +11,25 @@ class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    image_file = db.Column(db.String(200), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
+    def get_reset_token(self,expires = 1800):
+        s = Serializer(app.config['SECRET_KEY'],expires)
+        return s.dumps({'user:id':self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
